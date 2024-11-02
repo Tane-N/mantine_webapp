10ec898fd80d47da6b55646898e78d70d2e5932f
Taneli Nieminen <94614445+Tane-N@users.noreply.github.com>
2024-11-02 21:00:17 +0200

    header start

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,13 @@
 import "@mantine/core/styles.css";
-import { MantineProvider } from "@mantine/core";
-import { theme } from "./theme";
+import { Flex, MantineProvider } from "@mantine/core";
+import { Header } from "./Header.tsx";
 
 export default function App() {
-  return <MantineProvider theme={theme}>App</MantineProvider>;
+  return (
+    <MantineProvider defaultColorScheme="dark">
+      <Flex justify="center">
+        <Header></Header>
+      </Flex>
+    </MantineProvider>
+  );
 }
---
+++ src/Header.tsx
@@ -0,0 +1,37 @@
+import { useState } from "react";
+import { Container, Group, Burger } from "@mantine/core";
+import classes from "./Header.module.css";
+
+const links = [
+  { link: "/home", label: "Home" },
+  { link: "/about", label: "About" },
+  { link: "/projects", label: "Projects" },
+  { link: "/contact", label: "Contact" },
+];
+
+export function Header() {
+  const [active, setActive] = useState(links[0].link);
+
+  const items = links.map((link) => (
+    <a
+      key={link.label}
+      href={link.link}
+      className={classes.link}
+      data-active={active === link.link || undefined}
+      onClick={(event) => {
+        event.preventDefault();
+        setActive(link.link);
+      }}
+    >
+      {link.label}
+    </a>
+  ));
+
+  return (
+    <header className={classes.header}>
+      <Container size="md" className={classes.inner}>
+        <Group gap={5}>{items}</Group>
+      </Container>
+    </header>
+  );
+}
