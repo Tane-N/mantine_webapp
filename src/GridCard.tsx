8c08a314a0dd18fd3d9ee523abf4c0b0e67fd537
Tane <94614445+Tane-N@users.noreply.github.com>
2024-11-05 23:54:22 +0200

    swap to aspect ratio component

--- src/GridCard.tsx
+++ src/GridCard.tsx
@@ -23,22 +23,11 @@ export function GridCard(props: React.PropsWithoutRef<Props>) {
 
   const MIN_DESKTOP_WIDTH = 1280;
   const { width, height } = useViewportSize();
-  const isDesktop = useMediaQuery("(min-width: 1280px)");
 
-  const MODAL_GAP_MULTIPLIER = isDesktop ? 0.7 : 0.9;
-
-  const aspectRatio = 16 / 9;
-  const modalWidth = width * MODAL_GAP_MULTIPLIER;
-  const modalHeight = Math.min(
-    modalWidth / aspectRatio,
-    height * MODAL_GAP_MULTIPLIER
-  );
-
+  const ASPECT_RATIO = 16 / 9;
   const MAX_TEXT_SIZE = 35;
 
   const setTextSize = (): number => {
-    if (isDesktop) return MAX_TEXT_SIZE;
-
     return MAX_TEXT_SIZE * Math.min(Math.max(width / MIN_DESKTOP_WIDTH, 0), 1);
   };
 
@@ -51,7 +40,7 @@ export function GridCard(props: React.PropsWithoutRef<Props>) {
         }}
         className={classes.scale}
       >
-        <AspectRatio ratio={16 / 9}>
+        <AspectRatio ratio={ASPECT_RATIO}>
           <BackgroundImage
             src={props.image}
             h="100%"
@@ -83,7 +72,7 @@ export function GridCard(props: React.PropsWithoutRef<Props>) {
         zIndex={300}
         centered
         radius="md"
-        size={modalWidth}
+        size="80%"
         transitionProps={{
           duration: 150,
           transition: "scale",
@@ -92,42 +81,44 @@ export function GridCard(props: React.PropsWithoutRef<Props>) {
         padding={0}
         onClick={close}
       >
-        <BackgroundImage src={props.image} h={modalHeight} p="lg" radius="md">
-          <Flex h="100%" w="100%" justify="flex-end" align="flex-end">
-            <Image
-              src="./public/minimize-2.svg"
-              h="5%"
-              w="auto"
-              style={{ zIndex: 301 }}
-            />
-          </Flex>
-          <Center
-            style={{
-              position: "absolute",
-              height: "100%",
-              width: "100%",
-              top: 0,
-              left: 0,
-              zIndex: 300,
-            }}
-          >
-            <Title
-              size={setTextSize()}
-              ta="left"
-              c="white"
-              p="xl"
-              lh="xl"
+        <AspectRatio ratio={ASPECT_RATIO}>
+          <BackgroundImage src={props.image} h="100%" p="lg" radius="md">
+            <Flex h="100%" w="100%" justify="flex-end" align="flex-end">
+              <Image
+                src="./public/minimize-2.svg"
+                h="5%"
+                w="auto"
+                style={{ zIndex: 301 }}
+              />
+            </Flex>
+            <Center
               style={{
-                textShadow: "0.1rem 0.1rem 0.2rem rgba(0, 0, 0, 0.8)",
-                pointerEvents: "none",
-                userSelect: "none",
+                position: "absolute",
+                height: "100%",
+                width: "100%",
+                top: 0,
+                left: 0,
+                zIndex: 300,
               }}
             >
-              {props.content}
-            </Title>
-          </Center>
-        </BackgroundImage>
-        <Overlay color="#000" backgroundOpacity={0.6} radius="md" />
+              <Title
+                size={setTextSize()}
+                ta="left"
+                c="white"
+                p="xl"
+                lh="xl"
+                style={{
+                  textShadow: "0.1rem 0.1rem 0.2rem rgba(0, 0, 0, 0.8)",
+                  pointerEvents: "none",
+                  userSelect: "none",
+                }}
+              >
+                {props.content}
+              </Title>
+            </Center>
+          </BackgroundImage>
+          <Overlay color="#000" backgroundOpacity={0.6} radius="md" />
+        </AspectRatio>
       </Modal>
     </div>
   );
